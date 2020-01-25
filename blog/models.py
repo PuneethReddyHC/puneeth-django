67be54b3dc0c9c9d0f1fa4cca19e06b59ec091cd
Puneeth Reddy H C <puneethreddy951@gmail.com>
2020-01-25 11:47:25 +0530

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-from accounts.managers import UserManager
-from settings import AUTH_USER_MODEL
+from accounts.models import User
 STATUS = ((0, "Draft"), (1, "Publish"))
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="blog_posts")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     #author = models.ForeignKey(
      #   User, on_delete=models.CASCADE, related_name="blog_posts"
     #)
